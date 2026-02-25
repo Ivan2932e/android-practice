0143b13ce46fbaa7ae9077680680ffe9a4db2785
ivanf <ivanfilatovfell@gmail.com>
2026-02-26 01:33:17 +0300

    lesson1

---
+++ app/src/main/java/com/ivan_filatov/lesson1/MainActivity.java
@@ -0,0 +1,47 @@
+package com.ivan_filatov.lesson1;
+
+import android.os.Bundle;
+
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textViewStudent;
+    private Button btnWhoAmI;
+    private Button btnItIsNotMe;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textViewStudent = findViewById(R.id.textViewStudent);
+        btnWhoAmI = findViewById(R.id.btnWhoAmI);
+        btnItIsNotMe = findViewById(R.id.btnItIsNotMe);
+
+        View.OnClickListener oclBtnWhoAmI = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textViewStudent.setText("Филатов Иван, БСБО-54-24");
+            }
+        };
+        btnWhoAmI.setOnClickListener(oclBtnWhoAmI);
+
+        btnItIsNotMe.setOnClickListener(v ->
+                Toast.makeText(this, "It is not me!", Toast.LENGTH_SHORT).show()
+        );
+    }
+}
